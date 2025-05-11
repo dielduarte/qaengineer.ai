55eeb42d4c8611113bfe576e00ed9e8d80cd5a16
dielduarte <magdiel.kesller@gmail.com>
2025-05-11 00:19:35 -0300

    type args and make model-api-key required

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -2,17 +2,21 @@
 import yargs from 'yargs';
 import { hideBin } from 'yargs/helpers';
 
+interface CommandArgs {
+  'model-api-key': string;
+}
 
 yargs(hideBin(process.argv))
-.command('run', 'Execute the tests', () => {}, async (argv) => {
+  .command<CommandArgs>('run', 'Execute the tests', () => {}, async (argv) => {
     const {run} = await import('./commands/run.js');
 
     run({
-      apiKey: argv['model-api-key'] as string
+      apiKey: argv['model-api-key']
     });   
   })
   .option('model-api-key', {
     type: 'string',
     description: 'The API key for the model',
+    demandOption: true,
   })
   .parse()
