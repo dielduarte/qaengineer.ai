a0506fa2df8f3209d2f48dca2acf2d5a4a596959
dielduarte <magdiel.kesller@gmail.com>
2025-06-16 20:53:54 -0300

    fix ts errors

--- apps/website/src/components/video-hero.tsx
+++ apps/website/src/components/video-hero.tsx
@@ -8,10 +8,9 @@ export default function VideoHero() {
         height="100%"
         src="https://www.youtube.com/embed/0ejKPwVr9Q0?si=iotH4tn0GwXMSdnJ"
         title="YouTube video player"
-        frameborder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-        referrerpolicy="strict-origin-when-cross-origin"
-        allowfullscreen
+        referrerPolicy="strict-origin-when-cross-origin"
+        allowFullScreen
       ></iframe>
     </div>
   );
